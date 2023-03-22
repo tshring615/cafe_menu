333f4007b97dfa7767d94afe9fb99e3f6af04c3e
tshring615 <85819699+tshring615@users.noreply.github.com>
2023-03-22 21:41:10 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,69 +1,104 @@
 const menu = [
     {
     name: 'Americano',
-    category: 'Black Coffee',
+    category: 'Black-Coffee',
     tag: "HOT | ICED",
     desc: "Shot of espresso in hot water to shoot those blues.",
-    price: "NPR.95/- | NPR.125/-"
+    price: "95/- | 125/-"
 }, 
 {
-    name: 'Espresso',
-    category: 'Milk Coffee',
+    name: 'Latte',
+    category: 'Milk-Coffee',
     tag: "HOT",
     desc: "Power up your day with the single shot of espresso.",
-    price: "NPR.80/-"
+    price: "80/-"
+}, 
+{
+    name: 'Cappuccino',
+    category: 'Milk-Coffee',
+    tag: "HOT",
+    desc: "Power up your day with the single shot of espresso.",
+    price: "80/-"
 }
 ];
 
-const main = document.getElementById('main');
+let category = 'Black Coffee';
+
+window.addEventListener('DOMContentLoaded', loadFilteredMenu);
+const selectCategory = document.getElementById('category');
+selectCategory.addEventListener('change', (loadFilteredMenu));
+
+const main = document.getElementById('menu-container');
+const start = document.getElementById('main');
 
 
-    menu.forEach(menuItem => {
+    // let filteredMenu = menu.filter((items) => {
+    //     return items.category === category;   
+    // });
 
-        //Div element to contain individual menu items
-        const div = document.createElement('div');
-        div.setAttribute('id','container');
-        main.append(div);
-
-        //Div element to contain name, tag, desc
-        const title = document.createElement('div');
-        title.setAttribute('id','title');
-        div.append(title);
-
-        //Name of the menu Item
-        const name = document.createElement('h2');
-        name.setAttribute('id', 'name');
-        name.innerText = menuItem.name;
-        title.append(name);
+    function loadFilteredMenu() {
 
 
-        //Tag of the menu Item
-        const tag = document.createElement('p');
-        tag.setAttribute('id', 'tag');
-        tag.innerText = menuItem.tag;
-        title.append(tag);
+        category = selectCategory.value;
 
-        //Description of the menu Item
-        const description = document.createElement('p');
-        description.setAttribute('id', 'description');
-        if ('desc' in menuItem) {
-            description.innerText = menuItem.desc;
-            title.append(description);
-        } else {
-            description.remove()
-        }
-       
+        let filteredMenu = menu.filter((items) => {
+                return items.category === category;   
+            });
 
-        //Price of the menu item
-        const priceDiv = document.createElement('div');
-        priceDiv.setAttribute('id','price-container');
-        div.append(priceDiv);
-        
-        const price = document.createElement('p');
-        price.setAttribute('id', 'price'); 
-            price.innerText = menuItem.price;
-            priceDiv.append(price);
-      
-    });
+            filteredMenu.forEach(menuItem => {
+
+                //Div element to contain individual menu items
+             const div = document.createElement('div');
+             div.setAttribute('id','container');
+             div.classList.add(menuItem.category);
+             main.append(div);
+
+             //Div element to contain name, tag, desc
+             const title = document.createElement('div');
+             title.setAttribute('id','title');
+             div.append(title);
+     
+             //Name of the menu Item
+             const name = document.createElement('h2');
+             name.setAttribute('id', 'name');
+             name.innerText = menuItem.name;
+             title.append(name);
+     
+     
+             //Tag of the menu Item
+             const tag = document.createElement('p');
+             tag.setAttribute('id', 'tag');
+             tag.innerText = menuItem.tag;
+             title.append(tag);
+     
+             //Description of the menu Item
+             const description = document.createElement('p');
+             description.setAttribute('id', 'description');
+             if ('desc' in menuItem) {
+                 description.innerText = menuItem.desc;
+                 title.append(description);
+             } else {
+                 description.remove()
+             }
+            
+     
+             //Price of the menu item
+             const priceDiv = document.createElement('div');
+             priceDiv.setAttribute('id','price-container');
+             div.append(priceDiv);
+             
+             const price = document.createElement('p');
+             price.setAttribute('id', 'price'); 
+                 price.innerText = menuItem.price;
+                 priceDiv.append(price);
+
+     }); 
+            
+    }
 
 
+    
+
+
+
+   
